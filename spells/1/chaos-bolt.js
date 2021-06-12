ea1efb27b5d662bc813e7ec3e47f341e25a6b1dd
Maselkov <Maselkov@users.noreply.github.com>
2021-06-12 20:58:03 +0200

    Update Chaos Bolt

--- spells/1/chaos-bolt.js
+++ spells/1/chaos-bolt.js
@@ -11,9 +11,9 @@ const damage_types = [
   "Thunder",
 ];
 if (args[0].hitTargets.length > 0) {
-  let rolledTypes = args[0].damageRoll.terms[0].results.map(
-    (e) => damage_types[e.result - 1]
-  );
+  let rolledTypes = args[0].damageRoll.terms
+    .find((t) => t?.faces === 8)
+    .results.map((e) => damage_types[e.result - 1]);
   let types = [...new Set(rolledTypes)]; // Remove duplicates
   let bounce = types.length !== rolledTypes.length;
   let type;
@@ -27,14 +27,7 @@ if (args[0].hitTargets.length > 0) {
       `Chaos bolt bounces! Cast it again on another target within 30ft of the current one.`
     );
   }
-  let dice = args[0].spellLevel;
-  if (args[0].isCritical) {
-    dice *= 2;
-  }
-  let damageRoll = new Roll(
-    `${dice}d6+${args[0].damageRoll.results[0]}`
-  ).roll();
-  await game.dice3d.showForRoll(damageRoll);
+  let damageRoll = new Roll(`${args[0].damageRoll.total}`).roll();
   await new MidiQOL.DamageOnlyWorkflow(
     actor,
     token,
@@ -42,7 +35,7 @@ if (args[0].hitTargets.length > 0) {
     type,
     args[0].hitTargets.map((t) => canvas.tokens.get(t._id)),
     damageRoll,
-    { itemCardId: args[0].itemCardId }
+    { itemCardId: args[0].itemCardId, useOther: false }
   );
 }
 async function choose(options) {
