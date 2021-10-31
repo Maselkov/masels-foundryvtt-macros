bf146c4edb91575238335921a94d6754102d7f6a
Maselkov <Maselkov@users.noreply.github.com>
2021-10-31 16:39:21 +0100

    Add repelling blast

---
+++ spells/0/repelling-blast.js
@@ -0,0 +1,27 @@
+if (!args[0].hitTargets.length) { return };
+if (args[0].tag !== "DamageBonus") {
+    return;
+}
+if (!args[0].attackRoll) {
+    return;
+}
+if (args[0].item.name !== "Eldritch Blast") {
+    return;
+}
+new Dialog({
+    title: "Repelling Blast",
+    content: `<p>Use Repelling Blast to push the target back <b>10 Feet</b>?</p>`,
+    buttons: {
+        one: {
+            icon: '<i class="fas fa-check"></i>',
+            label: "Yes",
+            callback: async () => await game.macros.getName("Knockback").execute(canvas.tokens.get(args[0].tokenId), canvas.tokens.get(args[0].hitTargets[0].id), 10),
+        },
+        two: {
+            icon: '<i class="fas fa-times"></i>',
+            label: "No",
+        },
+    },
+    default: "one",
+}).render(true);
+
